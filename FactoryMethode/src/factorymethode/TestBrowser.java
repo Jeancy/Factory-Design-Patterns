8b3cc65d0d76d8a7c335ebf952dac7a90fa27199
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2020-03-30 06:22:33 -0700

    Adding another non-existing browser :"br5" and printing the null browser

--- FactoryMethode/src/factorymethode/TestBrowser.java
+++ FactoryMethode/src/factorymethode/TestBrowser.java
@@ -8,9 +8,11 @@ public class TestBrowser {
         Browser br2 = BrowserFactory.getBrowser("Google Chrome");
         Browser br3 = BrowserFactory.getBrowser("FireFox");
         Browser br4 = BrowserFactory.getBrowser("XT");
+        Browser br5 = BrowserFactory.getBrowser("NULL");
+        
         // Testing the object if it is not null before calling method on it to avoid NullPointerException.
         // Creating an array of browsers.
-        Browser [] brs = {br1, br2,br3, br4};
+        Browser [] brs = {br1, br2,br3, br4,br5};
         for(Browser br: brs){
             if (br != null) {
                br.start();
@@ -25,6 +27,7 @@ public class TestBrowser {
         ls.add(br2);
         ls.add(br3);
         ls.add(br4);
+        ls.add(br5);
         //using the forEch method and method reference as per java 8.
         ls.forEach(System.out::println);
     }
